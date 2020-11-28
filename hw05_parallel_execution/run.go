447110e332e79028656b1130c87d3ec58ee0b6e6
Vladimir Shulga <levsha.v.a@gmail.com>
2020-11-29 02:10:56 +0300

    HW5 is completed

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -2,14 +2,87 @@ package hw05_parallel_execution //nolint:golint,stylecheck
 
 import (
 	"errors"
+	"sync"
 )
 
 var ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
 
 type Task func() error
 
-// Run starts tasks in N goroutines and stops its work when receiving M errors from tasks
-func Run(tasks []Task, N int, M int) error {
-	// Place your code here
-	return nil
+// Функция выполнения задачи из очереди.
+func worker(tasksChn <-chan Task, tasksStatusChn chan<- error, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	// Запрашиваем задачу
+	tasksStatusChn <- nil
+
+	// Обрабатываем канал входящих задач
+	for task := range tasksChn {
+		if task == nil { // команда прекратить обработку
+			return
+		}
+
+		res := task()
+		tasksStatusChn <- res
+	}
+}
+
+// Функция для параллельного выполнения заданий переданным числом горутин.
+func Run(tasks []Task, workersCount int, maxErrors int) (result error) {
+	var tasksChn = make(chan Task)
+	var tasksStatusChn = make(chan error)
+
+	tasksFailed := 0
+	tasksToRun := len(tasks)
+	if workersCount > tasksToRun {
+		workersCount = tasksToRun
+	}
+
+	nextTaskNo := 0 // Номер следующей задачи на обработку
+	workInProgress := workersCount
+
+	var wg sync.WaitGroup
+	wg.Add(workersCount)
+
+	for i := 0; i < workersCount; i++ {
+		go worker(tasksChn, tasksStatusChn, &wg)
+	}
+
+	// Обрабатываем канал состояния задач
+	for err := range tasksStatusChn {
+		if err != nil {
+			if tasksFailed == maxErrors {
+				// Достигнут максимум ошибок, а он может быть и равен 0
+				result = ErrErrorsLimitExceeded
+				// Больше новых задач не раздаём
+				nextTaskNo = tasksToRun
+			}
+
+			tasksFailed++
+		}
+
+		// nil используется в качестве команды остановки обработчика - пропускаем такие значения
+		for nextTaskNo < tasksToRun && tasks[nextTaskNo] == nil {
+			nextTaskNo++
+		}
+
+		if nextTaskNo < tasksToRun {
+			tasksChn <- tasks[nextTaskNo]
+			nextTaskNo++
+		} else {
+			// Команда на прекращение работы
+			tasksChn <- nil
+			workInProgress--
+
+			// Всем отправлены команда на прекращение работы
+			if workInProgress == 0 {
+				break
+			}
+		}
+	}
+
+	// Ждём выхода из всех функция обработки
+	wg.Wait()
+
+	return
 }
